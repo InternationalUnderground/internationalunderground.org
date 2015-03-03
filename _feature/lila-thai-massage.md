c474366852fe8d4f387a0c3c481d045f70e010f9
gboone <boone.greg@gmail.com>
2015-03-03 08:25:12 -0500

    Front page logic reworked to use collections

---
+++ _feature/lila-thai-massage.md
@@ -0,0 +1,6 @@
+---
+title: Lila Thai Massage
+slug: lila-thai-massage
+posts:
+- lila-thai-massage
+---
--- _plugins/feature.rb
+++ _plugins/feature.rb
@@ -1,22 +1,90 @@
 # author:: Greg Boone boone.greg@gmail.com
 # license:: CC0
 module Jekyll
-	class FeaturedPostTag < Liquid::Tag
+	
+	class SeriesHelper
+		
+		def initialize(site)
+			@posts = site.posts
+			@feature = site.collections['feature'].docs
+		end
+
+		# Returns the series document matching a given slug
+		def series_by_slug(slug)
+			feature = @feature
+			return feature.find { |feature| feature.data['slug'] == slug }
+		end
+
+		# gets the feature corresponding to a post
+		def series_from_post(slug)
+			feature = @feature
+			return feature.find { |feature| feature.data['posts'].include?(slug) }
+		end
+		def get_page_slug(page)
+			return page['url'].split('/')[2]
+		end
+	end
+
+	class PostSeriesTag < Liquid::Tag
+		def initialize(tag_name, value, tokens)
+			@tag_name = tag_name
+			@value = value
+			@tokens = tokens
+		end
+		def render(context)
+			post = context.scopes[0]['post'].slug
+			helper =  SeriesHelper.new(context.registers[:site])
+			series = helper.series_from_post(post)
+			"Series: <a href=\"#{series.url}\">#{series.data['title']}</a>" unless series.nil?
+		end
+	end
+
+	class RelatedPostTag < Liquid::Tag
 		def initialize(tag_name, value, tokens)
 			super
 			@count = value.to_i
 		end
+		
 		def render(context)
 			site = context.registers[:site]
 			posts = site.posts
-			series = context.registers[:site].data['series']
-			feature = context.registers[:page]['feature']
-			slugs = series[feature]['posts']
-			featured_posts = Array.new()
-			posts.each { |x| featured_posts.push(x) if slugs.include?(x.slug) }
-			post = featured_posts[@count]
-			"<h3><a href=\"#{post.url}\">#{post.title}</a></h3><p>#{post.excerpt}<br /><a href=\"#{post.url}\">Continue Reading →</a></p>"
+			helper = SeriesHelper.new(site)
+			slug = site.config['featured_series']
+			series = helper.series_by_slug(slug)
+			post = posts.find { |post| post.slug == series.data['posts'][@count-1] }
+			"<h3><a href=\"#{post.url}\">#{post.title}</a></h3>
+			<p>#{post.excerpt}<br />
+			<a href=\"#{post.url}\">Continue reading →</a></p>"
+		end
+	end
+
+	class CurrentSeriesTag < Liquid::Tag
+		def initialize(tag_name, value, tokens)
+			super
+			@value = value.strip
+		end
+		def render(context)
+			value = @value
+			page = context.registers[:page]
+			helper = SeriesHelper.new(context.registers[:site])
+			slug = helper.get_page_slug(page)
+			series = helper.series_from_post(slug)
+			unless series.nil?
+				title = series.data['title']
+				url = series.data['slug']
+			end
+			if value == "title"
+				return title
+			elsif value == "url"
+				return url
+			elsif series.nil?
+				return context.registers[:site].data['title']
+			else
+				return series
+			end
 		end
 	end
 end
-Liquid::Template.register_tag('feature_post', Jekyll::FeaturedPostTag)
+Liquid::Template.register_tag('related_post', Jekyll::RelatedPostTag)
+Liquid::Template.register_tag('post_series', Jekyll::PostSeriesTag)
+Liquid::Template.register_tag('current_series', Jekyll::CurrentSeriesTag )
