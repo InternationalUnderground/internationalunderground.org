cb91cda1d0c73ca3bd59ae225395bffdbd7e61b4
gboone <boone.greg@gmail.com>
2015-03-26 18:24:31 -0400

    Date archives! Hooray!

---
+++ Gemfile
@@ -0,0 +1,2 @@
+gem 'jekyll', '3.0.0.pre.beta2'
+gem 'jekyll-archives', :git => 'https://github.com/jekyll/jekyll-archives.git'
---
+++ _plugins/authors.rb
@@ -0,0 +1,38 @@
+module Jekyll
+	class AuthorsTag < Liquid::Tag
+    def initialize(tag_name, value, tokens)
+      @tag_name = tag_name
+      @value = value
+      @tokens = tokens
+    end
+
+    def render(context)
+      page = context.registers[:page]
+      site = context.registers[:site]
+      pageauthors = page['author']
+      siteauthors = site.data['authors']
+      authors = []
+      unless pageauthors.nil?
+        for author in pageauthors
+          authors << siteauthors.find { |a| a['name'] == author }
+        end
+        return authors
+      end
+    end
+	end
+
+  module LookupFilter
+    def lookup(input, args)
+      args = args.split(',')
+      dataset = args[0].strip
+      key = args[1].strip unless args[1].nil?
+      data = Jekyll.sites[0].data[dataset]
+      lookup = data.find { |d| d['name'] == input }
+      return lookup[key] if key
+      return lookup
+    end
+  end
+end
+
+Liquid::Template.register_tag('page_authors', Jekyll::AuthorsTag)
+Liquid::Template.register_filter(Jekyll::LookupFilter)
